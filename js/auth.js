41063209ad5ff0fe5c5fde21019cb7d517a05f11
galfano50 <Helder_braga@outlook.com>
2025-03-30 09:30:03 -0300

    Add files via upload

---
+++ js/auth.js
@@ -0,0 +1,42 @@
+// Importar Firebase Auth
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js";
+
+const auth = getAuth();
+
+// Registrar novo usuário
+function register(email, password) {
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            console.log("Usuário registrado:", userCredential.user);
+        })
+        .catch((error) => {
+            console.error("Erro ao registrar:", error.message);
+        });
+}
+
+// Fazer login
+function login(email, password) {
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            console.log("Usuário logado:", userCredential.user);
+        })
+        .catch((error) => {
+            console.error("Erro ao logar:", error.message);
+        });
+}
+
+// Fazer logout
+function logout() {
+    signOut(auth)
+        .then(() => {
+            console.log("Usuário deslogado");
+        })
+        .catch((error) => {
+            console.error("Erro ao deslogar:", error.message);
+        });
+}
+
+// Expor funções globalmente para teste
+window.register = register;
+window.login = login;
+window.logout = logout;
---
+++ js/firebase-config.js
@@ -0,0 +1,15 @@
+// Configuração do Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyD_vM-tbAj_Gc2gv1XBa6GMOYpe9zNSv7s",
+    authDomain: "bar-do-truta-rpg-online.firebaseapp.com",
+    databaseURL: "https://bar-do-truta-rpg-online-default-rtdb.firebaseio.com",
+    projectId: "bar-do-truta-rpg-online",
+    storageBucket: "bar-do-truta-rpg-online.appspot.com",
+    messagingSenderId: "109187771317",
+    appId: "1:109187771317:web:13add20ef2fa8da2da07fc"
+};
+
+// Inicializar Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const rtdb = firebase.database();
