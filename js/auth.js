e3eb94ee89cfb2245e7d8361f3559f7b74ed0510
galfano50 <Helder_braga@outlook.com>
2025-03-30 13:03:43 -0300

    Add files via upload

--- js/auth.js
+++ js/auth.js
@@ -1,7 +1,6 @@
-// Importar Firebase Auth (versão 10)
+// Importar Firebase Auth
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js";
 
-// Configurar Firebase Auth
 const auth = getAuth();
 
 // Registrar novo usuário
@@ -41,11 +40,3 @@ function logout() {
 window.register = register;
 window.login = login;
 window.logout = logout;
-
-// Monitorar o estado de autenticação
-auth.onAuthStateChanged((user) => {
-    if (user) {
-        // Redireciona para a home após login bem-sucedido
-        window.location.href = "home.html";
-    }
-});
