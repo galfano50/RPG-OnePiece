5a3de8c9030df5b5d45b702fe221eeb2dc8c596f
galfano50 <Helder_braga@outlook.com>
2025-03-30 13:09:04 -0300

    Add files via upload

--- js/auth.js
+++ js/auth.js
@@ -40,3 +40,18 @@ function logout() {
 window.register = register;
 window.login = login;
 window.logout = logout;
+
+// Verificar se o usuário está logado ao carregar o site
+auth.onAuthStateChanged((user) => {
+    if (!user) {
+        // Se não houver um usuário logado, redirecionar para a página de login/cadastro
+        if (window.location.pathname !== "/index.html" && window.location.pathname !== "/") {
+            window.location.href = "index.html";  // Redireciona para o login/cadastro
+        }
+    } else {
+        // Se o usuário estiver logado, redirecionar para a página inicial ou home
+        if (window.location.pathname === "/index.html" || window.location.pathname === "/") {
+            window.location.href = "home.html";  // Redireciona para o home se já estiver logado
+        }
+    }
+});
